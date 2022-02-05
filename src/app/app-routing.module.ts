64d3b199a47af867ed1150e2fcfdfefc0f54d955
Daylton <dayltondb@gmail.com>
2022-02-04 21:33:22 -0300

    ROTA para LOGIN

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { LoginComponent } from './components/login/login.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { NavComponent } from './components/nav/nav.component';
 import { NgModule } from "@angular/core";
@@ -5,6 +6,7 @@ import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
+  {path: 'login', component: LoginComponent},
   {
     path: '', component: NavComponent, children: [
       {path: 'home', component: HomeComponent},
