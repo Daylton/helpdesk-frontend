58ae0c3382cea43d4596fb7afbd5c828511e4358
Daylton <dayltondb@gmail.com>
2022-02-05 03:33:55 -0300

    autenticacao de LOGIN

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,3 +1,5 @@
+import { Router } from "@angular/router";
+import { AuthService } from "./../../service/auth.service";
 import { Credenciais } from "./../../models/credenciais";
 import { Component, OnInit } from "@angular/core";
 import { FormControl, Validators } from "@angular/forms";
@@ -17,20 +19,29 @@ export class LoginComponent implements OnInit {
   email = new FormControl(null, Validators.email);
   senha = new FormControl(null, Validators.minLength(3));
 
-  constructor(private toastr: ToastrService) {}
+  constructor(
+    private toastr: ToastrService,
+    private service: AuthService,
+    private router: Router,
+  ) {}
 
   ngOnInit(): void {}
 
   logar() {
-    this.toastr.error("Usuário ou senha inválidos!", 'Login');
-    // this.creds.senha = '';
+    this.service.authenticate(this.creds).subscribe(
+      (resposta) => {
+        this.service.successfulLogin(
+          resposta.headers.get("Authorization").substring(7),
+        );
+        this.router.navigate([""]);
+      },
+      () => {
+        this.toastr.error("Usuário ou senha inválidos");
+      },
+    );
   }
 
   validaCampos(): boolean {
-    if (this.email.valid && this.senha.valid) {
-      return true;
-    } else {
-      return false;
-    }
+    return this.email.valid && this.senha.valid;
   }
 }
