930e85d4796201562624e396eb583a9e3e4daebc
Daylton <dayltondb@gmail.com>
2022-02-10 01:32:31 -0300

    Filtro e findAll para CHAMADO-LIST

--- src/app/components/chamado/chamado-list/chamado-list.component.ts
+++ src/app/components/chamado/chamado-list/chamado-list.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
+import { ChamadoService } from "./../../../services/chamado.service";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatSort } from "@angular/material/sort";
+import { MatTableDataSource } from "@angular/material/table";
 
-import { Chamado } from './../../../models/chamado';
+import { Chamado } from "./../../../models/chamado";
 
 @Component({
   selector: "app-chamado-list",
@@ -11,34 +12,8 @@ import { Chamado } from './../../../models/chamado';
   styleUrls: ["./chamado-list.component.scss"],
 })
 export class ChamadoListComponent implements OnInit {
-  DATA: Chamado[] = [
-    {
-      id: 1,
-      dataAbertura: "05/02/2022",
-      dataFechamento: "08/02/2022",
-      prioridade: "ALTA",
-      status: "ANDAMENTO",
-      titulo: "Instalação Impressora",
-      descricao: "Teste Chamado",
-      tecnico: 1,
-      cliente: 1,
-      nomeCliente: "Jorge",
-      nomeTecnico: "Daylton",
-    },
-    {
-      id: 2,
-      dataAbertura: "05/02/2022",
-      dataFechamento: "08/02/2022",
-      prioridade: "MEDIA",
-      status: "CONCLUIDO",
-      titulo: "Instalação Impressora",
-      descricao: "Teste Chamado",
-      tecnico: 1,
-      cliente: 1,
-      nomeCliente: "Jorge",
-      nomeTecnico: "Pedro",
-    },
-  ];
+  DATA: Chamado[] = [];
+  FILTERED_DATA: Chamado[] = [];
 
   displayedColumns: string[] = [
     "id",
@@ -55,17 +30,62 @@ export class ChamadoListComponent implements OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  constructor() {}
+  constructor(private service: ChamadoService) {}
 
-  ngOnInit(): void {}
-
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+  ngOnInit(): void {
+    this.findAll();
+    // this.ngAfterViewInit();
   }
+
+  findAll(): void {
+    this.service.findAll().subscribe((resposta) => {
+      this.DATA = resposta;
+      this.dataSource = new MatTableDataSource<Chamado>(resposta);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+  }
+
+  // ngAfterViewInit() {
+  //   this.dataSource.paginator = this.paginator;
+  //   this.dataSource.sort = this.sort;
+  // }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  retornaStatus(status: any): string {
+    if (status == "0") {
+      return "ABERTO";
+    } else if (status == "1") {
+      return "EM ANDAMENTO";
+    } else {
+      return "ENCERRADO";
+    }
+  }
+
+  retornaPrioridade(prioridade: any): string {
+    if (prioridade == "0") {
+      return "BAIXA";
+    } else if (prioridade == "1") {
+      return "MÉDIA";
+    } else {
+      return "ALTA";
+    }
+  }
+
+  orderByStatus(status: any): void {
+    let list: Chamado[] = [];
+    this.DATA.forEach((element) => {
+      if (element.status == status) {
+        list.push(element);
+        console.log(element.status);
+      }
+    });
+    this.FILTERED_DATA = list;
+    this.dataSource = new MatTableDataSource<Chamado>(list);
+    this.dataSource.paginator = this.paginator;
   }
 }
